a1841a13af33ba2d065c4a5fdd71a3417e4732e7
Jack Li <XT.li@student.han.nl>
2024-03-13 11:16:50 +0100

    initial commit

---
+++ src/main/java/com/github/hanyaeger/tutorial/Alienjump.java
@@ -0,0 +1,19 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Alienjump extends YaegerGame {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
